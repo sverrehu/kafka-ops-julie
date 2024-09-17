c40a432fdf2605b04d9921655f90ff186857356c
Sverre H. Huseby <shh@thathost.com>
2024-09-17 15:43:59 +0200

    chore: delete stuff not used since this project had a different name (#64)
    * chore: delete stuff not used since this project had a different name
    
    * put back file that was in use

--- release/deb/sign-deb.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-FILE=$1
-IDENTITY=${2:-"393416696134E892"}
-
-export GPG_TTY=$(tty)
-dpkg-sig -k $IDENTITY --sign builder $FILE
--- release/docker/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cp  ../../target/julie-ops.jar .
-cp ../../src/main/scripts/julie-ops-cli.sh .
-
-docker build --compress -t purbon/kafka-topology-builder .
-
-rm julie-ops.jar
-rm julie-ops-cli.sh
